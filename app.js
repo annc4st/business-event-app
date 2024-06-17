05b9a4d9345c41b0a518b4bcd6addbdfecb626e6
annc4st <annaseniuta@gmail.com>
2024-06-17 21:37:41 +0100

    cors

--- app.js
+++ app.js
@@ -17,7 +17,16 @@ const multer = require('multer');
 
 
 const app = express();
-app.use(cors());
+
+ 
+
+  const corsOptions ={
+    origin: ['http://localhost:3000', 'https://business-events.netlify.app/'],
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+ 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
