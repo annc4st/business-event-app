0effd0160133b57763c879bcfd53e37d9d349dec
annc4st <annaseniuta@gmail.com>
2024-06-17 20:27:49 +0100

    edited cors

--- app.js
+++ app.js
@@ -17,10 +17,7 @@ const multer = require('multer');
 
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
