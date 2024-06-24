807e80935ba062195d774a1bfcdc246e9a61ec80
annc4st <annaseniuta@gmail.com>
2024-06-24 14:55:59 +0100

    removed comments

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -14,19 +14,19 @@ authRouter.get('/users', getAllUsers);
 authRouter.post('/login', (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
       if (err) {
-        console.error('Error during authentication:', err);  // Add this line
+        console.error('Error during authentication:', err); 
         return next(err);
       }
       if (!user) {
-        console.log('Authentication failed:', info);  // Add this line
+        // console.log('Authentication failed:', info); 
         return res.status(400).json({ message: info.message });
       }
       req.logIn(user, (err) => {
         if (err) {
-          console.error('Error during login:', err);  // Add this line
+          console.error('Error during login:', err);  
           return next(err);
         }
-        return login(req, res);  // Call the login controller function
+        return login(req, res);  // call the login controller function
       });
     })(req, res, next);
   });
