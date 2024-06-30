2b3a6e4e56d3b1e3dc418fa651f0c8bf55dffe2e
annc4st <annaseniuta@gmail.com>
2024-06-30 11:18:38 +0100

    removed ensure auth

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -7,9 +7,9 @@ const { register, login, logout, getUser, getAllUsers, getUserSignedUpEvents } =
 
 authRouter.post('/register', register);
 
-authRouter.post('/logout', ensureAuthenticated, logout);
-authRouter.get('/profile', ensureAuthenticated, getUser);
-authRouter.get('/profile/events', ensureAuthenticated, getUserSignedUpEvents);
+authRouter.post('/logout', logout); //ensureAuthenticated, 
+authRouter.get('/profile', getUser);
+authRouter.get('/profile/events', getUserSignedUpEvents);
 authRouter.get('/users', getAllUsers);
 authRouter.post('/login', (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
