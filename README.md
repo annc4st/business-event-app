34edbdd3fc5ce3bc7eba90e1ae94996f3e535321
annc4st <annaseniuta@gmail.com>
2024-09-04 20:04:10 +0100

    deleting comments

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### Notes
 I am currently working on moving frontend and backend to the same hosting service (my authorization did not work properly because of that)
-I am also switching from posgresql to mongodb due to the end of life of the elephantsql.
+I am also looking for new postgresql db due to the end of life of the elephantsql.
 
 More notes to follow...
--- models/util_func.js
+++ models/util_func.js
@@ -1,14 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 const convertToUTC = (date, time) => {
-    // const dateTime = new Date(new Date(`${date}T${time}Z`).getTime() - new Date(`${date}T${time}Z`).getTimezoneOffset() *60000).toISOString();
-    // return dateTime;
     const localDateTime = new Date(`${date}T${time}`);
     const offset = localDateTime.getTimezoneOffset() * 60000;
     const utcDateTime = new Date(localDateTime.getTime() - offset).toISOString();
     return utcDateTime;
   }
 
+  
   const createToken = (id) => {
     return jwt.sign({ id }, process.env.SECRET, { expiresIn: "3h" });
   };
