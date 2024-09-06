1d52ec0a2920523393b4eeb592326552940eb799
annc4st <annaseniuta@gmail.com>
2024-09-06 12:46:24 +0100

    some changes connectionjs

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -9,14 +9,26 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE not set');
+// Check for required environment variables
+if (!process.env.PGDATABASE || !process.env.PGUSER || !process.env.PGPASSWORD || !process.env.PGHOST) {
+  throw new Error('Required environment variables are not set');
 }
 
-const config = {};
-if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+// const config = {};
+// if (ENV === 'production') {
+//   config.connectionString = process.env.DATABASE_URL;
+//   config.max = 2;
+// }
+
+//Configure the postgresql client
+const config = {
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT || 5432, // Use default port if not specified
+  // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // Enable SSL in production
+  ssl: false,
 }
 
 module.exports = new Pool(config);
