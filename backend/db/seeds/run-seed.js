cb7c55210705ada9eb3b3d825811804180e887f9
annc4st <annaseniuta@gmail.com>
2024-09-14 21:31:20 +0100

    changes in s

--- backend/db/seeds/run-seed.js
+++ backend/db/seeds/run-seed.js
@@ -1,5 +1,6 @@
 const devData = require('../data/development-data/index.js');
-const seed = process.env.NODE_ENV === 'production' ? require('./seed-prod.js') : require('./seed.js');
+const seed = process.env.NODE_ENV === 'production'
+ ? require('./seed-prod.js') : require('./seed.js');
 const db = process.env.NODE_ENV === 'production' ? require('../sqlConnection') : require('../connection');
 
 
@@ -7,19 +8,12 @@ const db = process.env.NODE_ENV === 'production' ? require('../sqlConnection') :
 //   return seed(devData).then(() => db.end());
 // };
 
-
-const runSeed = async () => {
-  try {
-    // Run the seed process with development data
-    await seed(devData);
-  } catch (error) {
-    console.error('Error running seed:', error);
-  } finally {
-    // Ensure the database connection is closed properly for non-production environments
-    if (process.env.NODE_ENV !== 'production') {
-      await db.end();
-    }
-  }
+const runSeed = () => {
+  return seed(devData)
+    .then(() => db.end())
+    .catch((err) => {
+      console.error(`Error seeding ${process.env.NODE_ENV} database:`, err);
+    });
 };
 
 
