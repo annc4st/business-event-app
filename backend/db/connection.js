3a66d8dcfb808631b07bd8fac051ca3d5697c47b
annc4st <annaseniuta@gmail.com>
2024-09-16 21:24:27 +0100

    fixed prod db connect

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,28 +1,38 @@
 const { Pool } = require('pg');
 
-
+const ENV = process.env.NODE_ENV || 'development';
 require('dotenv').config({
   path: `${__dirname}/../.env.${process.env.NODE_ENV || 'development'}`,
 });
 
-// Check for required environment variables
-if (!process.env.PGDATABASE || !process.env.PGUSER || !process.env.PGPASSWORD || !process.env.PGHOST) {
-  throw new Error('Required environment variables are not set');
+
+if ((ENV === 'development' && (!process.env.PGDATABASE || !process.env.PGUSER || !process.env.PGPASSWORD || !process.env.PGHOST)) 
+  || (ENV === 'production' && !process.env.DATABASE_URL)) {
+throw new Error('Required environment variables are not set');
 }
  
+const config = {};
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
 
 //Configure the postgresql client
-const config = {
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT || 5432, // Use default port if not specified
+// const config = {
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT || 5432,  
+//   ssl: {
+//     rejectUnauthorized: false, 
+//   },
 
-}
+// }
 
-if (process.env.NODE_ENV === 'production') {
-  config.ssl = { rejectUnauthorized: false };
-}
+// if (process.env.NODE_ENV === 'production') {
+//   config.ssl = { rejectUnauthorized: false };
+// }
 
 module.exports = new Pool(config);
--- backend/db/seeds/run-seed.js
+++ backend/db/seeds/run-seed.js
@@ -1,20 +1,10 @@
 const devData = require('../data/development-data/index.js');
-const seed = process.env.NODE_ENV === 'production'
- ? require('./seed-prod.js') : require('./seed.js');
-const db = process.env.NODE_ENV === 'production' ? require('../sqlConnection') : require('../connection');
+const seed = require('./seed.js');
+const db = require('../connection.js')
 
-
-// const runSeed = () => {
-//   return seed(devData).then(() => db.end());
-// };
 
 const runSeed = () => {
-  return seed(devData)
-    .then(() => db.end())
-    .catch((err) => {
-      console.error(`Error seeding ${process.env.NODE_ENV} database:`, err);
-    });
+  return seed(devData).then(() => db.end());
 };
-
 
 runSeed();
