e334aede95b2d9c800995cca40704732872efb7c
annc4st <annaseniuta@gmail.com>
2024-11-30 14:08:03 +0000

    changed upload image func - moved to api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -41,6 +41,25 @@ export const postEvent = async (newEvent) => {
     }
 }
 
+// image upload
+export const imageUpload = async (file, setFieldValue) => {
+    const formData = new FormData();
+    formData.append('image', file);
+    try {
+        const response = await eventsApi.post(
+            `/uploads/upload`,
+            formData, {
+                headers: { 'Content-Type': 'multipart/form-data',
+                },
+            });
+            return response.data.imagePath; // Return the image path 
+            
+    } catch (error){
+        console.error('Error uploading image:', error);
+        setError('Image upload failed');
+    }
+};
+
 //add user to guestlist for event when user signs up
 export const addGuest = (event_id, userId, token) => {
     return eventsApi.patch(`/events/${event_id}/guests`,  { id: userId }, {
